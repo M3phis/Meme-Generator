eeac7a73314516ec65ba750029daf83896e69180
Tomer <tomera.almog9@gmail.com>
2024-12-19 20:58:56 +0200

    Init Repo + first setup

---
+++ js/controllers/gallery.controller.js
@@ -0,0 +1,18 @@
+'use strict'
+
+function renderGallery() {
+  const elGallery = document.querySelector('.image-container')
+  const imgs = getImgs()
+  let strHTML
+
+  strHTML = imgs.map(
+    (image) => `
+    <img src="${image.url}" alt="image">
+    `
+  )
+
+  elGallery.innerHTML += strHTML
+  console.log(strHTML)
+}
+
+renderGallery()
